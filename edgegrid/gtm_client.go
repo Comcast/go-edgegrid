76c06e615e2a9205c63a19028b1a293eae58e746
Mike Ball <mikedball@gmail.com>
2016-05-17 21:37:57 -0400

    add comments to gtm_client.go

--- edgegrid/gtm_client.go
+++ edgegrid/gtm_client.go
@@ -2,15 +2,19 @@ package edgegrid
 
 import "net/http"
 
+// GTMClient is an Akamai GTM API client
+// https://developer.akamai.com/api/luna/config-gtm/overview.html
 type GTMClient struct {
 	Credentials *AuthCredentials
 	HttpClient  *http.Client
 }
 
+// GetCredentials takes a GTMClient and returns its Credentials
 func (c GTMClient) GetCredentials() *AuthCredentials {
 	return c.Credentials
 }
 
+// GetHttpClient takes a GTMClient and returns its HttpClient
 func (c GTMClient) GetHttpClient() *http.Client {
 	return c.HttpClient
 }
