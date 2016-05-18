cf52194466703eb827406726e31e8159551203b9
Mike Ball <mikedball@gmail.com>
2016-05-17 22:26:27 -0400

    add comments to papi_client.go

--- edgegrid/papi_client.go
+++ edgegrid/papi_client.go
@@ -2,15 +2,19 @@ package edgegrid
 
 import "net/http"
 
+// PAPIClient is an Akamai PAPI API client
+// https://developer.akamai.com/api/luna/papi/overview.html
 type PAPIClient struct {
 	Credentials *AuthCredentials
 	HttpClient  *http.Client
 }
 
+// GetCredentials takes a PAPIClient and returns its credentials
 func (c PAPIClient) GetCredentials() *AuthCredentials {
 	return c.Credentials
 }
 
+// GetHttpClient takes a PAPIClient and returns its HttpClient
 func (c PAPIClient) GetHttpClient() *http.Client {
 	return c.HttpClient
 }
